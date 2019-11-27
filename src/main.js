c800b5ffd34ce50819e81140fb62c5d94fe1da2f
Joshua Mora <joshuaalexandermora@gmail.com>
2019-11-27 14:53:52 -0500

    Project creation and configuration

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import { Persistence } from './services/persistence'
+import Utils from './services/utils'
+import axios from 'axios'
+import { Config } from './services/config.js'
+import ProximaxProvider from './services/proximaxProvider'
+
+let currentNode = localStorage.getItem('currentNode')
+
+Vue.config.productionTip = false
+Vue.prototype.$localStorage = new Persistence(localStorage)
+Vue.prototype.$sessionStorage = new Persistence(sessionStorage)
+Vue.prototype.$utils = Utils
+
+const configIntegration = async (currentNodeExist = false) => {
+  try {
+    let configInfo = await axios.get('../config/config.json')
+    Vue.prototype.$config = new Config(configInfo.data)
+    if (currentNodeExist === false) {
+      localStorage.setItem('currentNode', configInfo.data.Nodes[0])
+      Vue.prototype.$proxProvider = new ProximaxProvider(configInfo.data.Nodes[0])
+    } else {
+      Vue.prototype.$proxProvider = new ProximaxProvider(currentNode)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
+  new Vue({
+    router,
+    store,
+    render: function (h) { return h(App) }
+  }).$mount('#app')
+}
+
+if (currentNode === null) {
+  console.log('No Current Node')
+  configIntegration(false)
+} else {
+  console.log('Current Node is', currentNode)
+  configIntegration(true)
+}
+
+new Vue({
+  router,
+  store,
+  render: function (h) { return h(App) }
+}).$mount('#app')
---
+++ src/services/config.js
@@ -0,0 +1,13 @@
+class Config {
+  constructor (configObject) {
+    this.coin = configObject.Coin
+    this.nodes = configObject.Nodes
+    this.networkType = configObject.NetworkType
+    this.rentalFeeInfo = configObject.RentalFeeInfo
+    this.mapsInfo = configObject.MapsInfo
+    this.version = configObject.Version
+    this.moduleActive = configObject.ModuleActive
+  }
+}
+
+export { Config }
