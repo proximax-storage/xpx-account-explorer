8ecc2879d71d753984ab3d0e9ed6495de98f6682
Joshua Mora <joshuaalexandermora@gmail.com>
2019-12-09 15:05:45 -0500

    Transaction module added

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -10,10 +10,50 @@
           <label for="fileSelect" class="proximax-btn">Choose file</label>
           <div class="mt-10 fold" v-if="fileName !== null">{{ fileName }}</div>
         </form>
+
+        <div class="mt-10 fold">
+          <input type="button" @click="changeState">
+        </div>
       </div>
 
-      <div>
+      <div v-if="fileExist === null">
         <h1 class="title txt-left">Example table</h1>
+        <table class="table-setting">
+          <tr>
+            <th class="txt-left">Sender</th>
+            <th class="txt-left">Recipient</th>
+            <th class="txt-left">Address</th>
+            <th class="txt-left">Mosaic</th>
+            <th class="txt-left">Value</th>
+          </tr>
+          <tr>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+          </tr>
+        </table>
+      </div>
+
+      <div v-if="fileExist !== null">
+        <h1 class="title txt-left">File table</h1>
+        <table class="table-setting">
+          <tr>
+            <th class="txt-left">Sender</th>
+            <th class="txt-left">Recipient</th>
+            <th class="txt-left">Address</th>
+            <th class="txt-left">Mosaic</th>
+            <th class="txt-left">Value</th>
+          </tr>
+          <tr>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+            <td class="txt-left">hola</td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
@@ -32,7 +72,8 @@ export default {
   data () {
     return {
       moduleName: 'Transactions (CSV file)',
-      fileName: null
+      fileName: null,
+      fileExist: null
     }
   },
 
@@ -43,6 +84,10 @@ export default {
         console.log(event.target.files[0].name)
         this.fileName = event.target.files[0].name
       }
+    },
+
+    changeState () {
+      this.fileExist = (this.fileExist === null) ? 'algo' : null
     }
   }
 }
