bccf8051e73926de192b42c9dbd3625d68fc612e
Joshua Mora <joshuaalexandermora@gmail.com>
2019-12-12 10:25:16 -0500

    Changes in transaction

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -12,6 +12,15 @@
         </form>
       </div>
 
+      <div class="box-grey mb-10">
+        <h1 class="title txt-left">Account selection</h1>
+        <select v-if="myAccounts !== null || myAccounts.length > 0" name="Select Account" id="chooseAcc" placeholder="Select Account" class="proximax-btn-white">
+          <option value="" v-for="(item, index) in myAccounts" :key="index">
+            {{ item.name }}
+          </option>
+        </select>
+      </div>
+
       <div v-if="fileExist !== null">
         <h1 class="title txt-left">File table</h1>
         <table class="table-setting">
@@ -51,8 +60,11 @@
         </table>
       </div>
     </div>
+
     <div class="mt-10 fold">
-      <input type="button" @click="sendTx" value="Send">
+      <div class="box-grey">
+        <input type="button" class="proximax-btn" @click="sendTx" value="Send">
+      </div>
     </div>
   </div>
 </template>
@@ -74,14 +86,17 @@ export default {
       fileExist: null,
       sortOrders: {},
       parse_csv: [],
-      parse_header: []
+      parse_header: [],
+      myAccounts: JSON.parse(this.$localStorage.get('myAccounts'))
     }
   },
+
   filters: {
     capitalize: function (str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
     }
   },
+
   methods: {
     readFile (event) {
       if ([undefined, null].includes(event.target.files) === false) {
@@ -93,7 +108,7 @@ export default {
       let lines = csv.split('\n')
       let result = []
       let headers = lines[0].split(',')
-      headers = headers.map((x) => x.toUpperCase())
+      headers = headers.map((x) => x.toLowercase())
       this.parse_header = lines[0].split(',')
       this.parse_header = this.parse_header.map((x) => x.toUpperCase())
 
