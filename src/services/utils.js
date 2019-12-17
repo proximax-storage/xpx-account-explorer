53b1c1152cfdec44f48df6032c1bdce83be64dee
Joshua Mora <joshuaalexandermora@gmail.com>
2019-12-17 14:30:23 -0500

    FormatTransfer in utils

--- src/services/utils.js
+++ src/services/utils.js
@@ -439,6 +439,7 @@ export default class Utils {
     }
     return arrayData
   }
+
   /**
    * Method validate account type and  TX type
    * @param account account select sign
@@ -459,6 +460,7 @@ export default class Utils {
     }
     // account
   }
+
   static buildHashLockTransaction (signedTransaction, signer, generationHash, config) {
     const hashLockTransaction = HashLockTransaction.create(
       Deadline.create(),
@@ -468,7 +470,23 @@ export default class Utils {
       config.network.number)
     return signer.sign(hashLockTransaction, generationHash)
   }
+
   static isMultisig (multisigInfo) {
     return multisigInfo.minRemoval !== 0 && multisigInfo.minApproval !== 0
+  }
+
+  static formatTransaction (transactions, config, provider) {
+    console.log(transactions, transactions.length)
+    console.log('OTHERS', config, provider)
+
+    for (let i = 0; i < transactions.length; i++) {
+      console.log(transactions[i])
+
+      let tmpObj = {
+        address: transactions[i].signer.address.pretty()
+      }
+
+      console.log(tmpObj)
+    }
   }
 }
